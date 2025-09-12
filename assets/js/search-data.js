18d459f9074d5ce77a20434c2aaf315966c32ab8
TatianaIM <130076080+TatianaIM@users.noreply.github.com>
2025-09-12 09:47:52 +0000

    Deploying to gh-pages from @ TatianaIM/TatianaIM.github.io@2ef280055a032930556e49b3327e4a384cfddb32 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -411,49 +411,19 @@ ninja.data = [{
               window.location.href = "/news/announcement_1/";
             },},{id: "projects-disordered-proteins-and-polypeptides",
           title: 'Disordered Proteins and Polypeptides',
-          description: "Why and How Certain Proteins Phase Separate?",
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-soft-matter-at-interfaces",
+          title: 'Soft Matter at Interfaces',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-design-of-structural-polymer-colloids",
+          title: 'Design of Structural Polymer Colloids',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
             },},{
         id: 'social-email',
         title: 'email',
