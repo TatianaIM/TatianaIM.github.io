76a94b7fa7db191e1bcdca1fea6770c1c18a0f6b
TatianaIM <130076080+TatianaIM@users.noreply.github.com>
2026-04-06 18:41:59 +0000

    Deploying to gh-pages from @ TatianaIM/TatianaIM.github.io@e82c7ef84ec0dcd4533939833cbdca7dcc9191ea 🚀

--- _pages/fran_toplek.md
+++
@@ -1,3 +0,0 @@
-### Fran B. Toplek
-
-Postdoctoral researcher working on the development of transferable coarse-grained models for biomolecular condensates.
--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-people",
           title: "people",
-          description: "group members and collaborators",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/people/";
